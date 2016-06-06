33a530021a4e5303bf8c6d2c1f9f7270761a9e5f
Alberto Santini <santini.alberto@gmail.com>
2016-06-06 14:01:57 +0200

    Fixed cardinality preserving

--- src/relaxations/cardinality_preserving.c
+++ src/relaxations/cardinality_preserving.c
@@ -25,8 +25,6 @@ struct relaxation_solution solve_cardinality_preserving_relaxation_for_bounds(co
 
   for(ptrdiff_t i = 0; i < inst->n_items; i++) { sol.coeff[i] = 0.0; }
 
-  ptrdiff_t last_inserted = ids[0];
-
   get_monotonic_time(&start);
 
   while(i < inst->n_items) {
@@ -43,18 +41,8 @@ struct relaxation_solution solve_cardinality_preserving_relaxation_for_bounds(co
     sol.coeff[ids[i]] = 1.0;
     sol.used_capacity += inst->weights[ids[i]];
     sol.profit += inst->profits[ids[i]];
-    last_inserted = ids[i];
 
     i++;
-  }
-
-  if(sol.used_capacity < inst->capacity) {
-    double residual_capacity = inst->capacity - sol.used_capacity;
-    double item_weight = inst->weights[last_inserted];
-
-    sol.coeff[last_inserted] += residual_capacity / item_weight;
-    sol.profit += residual_capacity * inst->profits[last_inserted] / item_weight;
-    sol.used_capacity = inst->capacity;
   }
 
   get_monotonic_time(&end);
@@ -66,6 +54,7 @@ struct relaxation_solution solve_cardinality_preserving_relaxation_for_bounds(co
 
 struct relaxation_solution solve_cardinality_preserving_relaxation(const struct instance* inst) {
   ptrdiff_t* ids;
+  double* profits;
 
   ids = malloc(inst->n_items * sizeof(*ids));
   if(ids == NULL) {
@@ -73,8 +62,17 @@ struct relaxation_solution solve_cardinality_preserving_relaxation(const struct 
     exit(EXIT_FAILURE);
   }
 
-  for(ptrdiff_t i = 0; i < inst->n_items; i++) { ids[i] = i; }
-  heapsort_desc(inst->pws, ids, inst->n_items);
+  profits = malloc(inst->n_items * sizeof(*ids));
+  if(profits == NULL) {
+    printf("Cannot allocate memory for dbl profits\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for(ptrdiff_t i = 0; i < inst->n_items; i++) {
+    ids[i] = i;
+    profits[i] = (double)inst->profits[i];
+  }
+  heapsort_desc(profits, ids, inst->n_items);
 
   uint_fast32_t maxb = (inst->n_black < inst->n_white ? inst->n_black : inst->n_white + 1);
   uint_fast32_t maxw = (inst->n_black < inst->n_white ? inst->n_black + 1 : inst->n_white);
