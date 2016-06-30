3b4888dcc69b657a79d81691a87aca445af423b8
Alberto Santini <a.santini@unibo.it>
2016-06-30 16:37:40 +0200

    Fixed cluster script

--- opt/cluster_scripts/launch_mip.sh
+++ opt/cluster_scripts/launch_mip.sh
@@ -16,5 +16,5 @@ cat <<EOF >${scriptname}
 for f in ${dir}*.txt ; do ${exec} \${f} testmip ${outfile} ; done
 EOF
     
-    oarsub -n "${klass}" -O "${klass}.out" -E "${klass}.err" -p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=1,walltime=12 "${scriptname}"
+    oarsub -n "${klass}" -O "${klass}.out" -E "${klass}.err" -p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=1,walltime=12 "./${scriptname}"
 done
