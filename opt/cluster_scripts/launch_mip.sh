89ef769b6aab9896794bf0779e01353b819380aa
Alberto Santini <a.santini@unibo.it>
2016-06-30 16:39:01 +0200

    Added exec permission to auxiliary scripts

--- opt/cluster_scripts/launch_mip.sh
+++ opt/cluster_scripts/launch_mip.sh
@@ -16,5 +16,6 @@ cat <<EOF >${scriptname}
 for f in ${dir}*.txt ; do ${exec} \${f} testmip ${outfile} ; done
 EOF
     
+    chmod u+x ${scriptname}    
     oarsub -n "${klass}" -O "${klass}.out" -E "${klass}.err" -p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=1,walltime=12 "./${scriptname}"
 done
