f16f005145d56b858fde907189c94fdf77fadbdb
Alberto Santini <a.santini@unibo.it>
2016-06-30 16:34:22 +0200

    Added cluster script

---
+++ opt/cluster_scripts/launch_mip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+resultsdir="../../results"
+exec="../../build/fast-bwkp"
+
+mkdir -p ${resultsdir}
+
+for dir in $(ls -d ../../data/*/)
+do
+    klass="$(basename ${dir})"
+    outfile="${resultsdir}/${klass}.txt"
+    scriptname="launch_${klass}.sh"
+    
+cat <<EOF >>${scriptname}
+#!/bin/bash
+for f in ${dir}*.txt ; do ${exec} \${f} testmip ${outfile} ; done
+EOF
+    
+    oarsub -n "${klass}" -O "${klass}.out" -E "${klass}.err" -p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=1,walltime=12 "${scriptname}"
+    
+    rm ${scriptname}
+done
